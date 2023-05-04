0826ff3842d7ce3d0b2102a4d1446a7b856379a3
Christian Alexander <christianalexander@christians-mbp.lan>
2023-05-03 23:15:53 -0700

    palindrome solved

--- solutions.js
+++ solutions.js
@@ -3,16 +3,51 @@
 //////////////////////
 // 1.
 
-// The difference between a parameter and a argument is a parameter is a variable that is 
-// defined to represent the argument(s) in the function while the argument is the actual 
+// The difference between a parameter and a argument is a parameter is a variable that is
+// defined to represent the argument(s) in the function while the argument is the actual
 // data set.
 
-// 2. 
+// 2.
 
-// The difference between a console log and the return is that console log only logs the 
+// The difference between a console log and the return is that console log only logs the
 // value we are asking for and isn't generally useful, where as return takes the value
 // we are creating inside of a function that we can use in our main programs.
 
 //////////////////////
 // Section 2
+//////////////////////
+
+// // define the function
+// const checkPalindrome = (str) => {
+//     // I want to set the string to lower case and replace 
+//     // unnecessary characters of the string
+//     str = str.toLowerCase().replace(/\W/g, '');
+//     // loop over every letter in the string
+//   for (let i = 0; i < str.length; i += 1) {
+//     // A conditional that determines a false palindrome
+//     if (str[i] !== str[str.length - 1 - i]) {
+//       return "Is not a Palindrome";
+//     }
+//   }
+//   // return true if it is a palindrome
+//   return "Is a Palindrome";
+// };
+
+// let str1 = "RaceCar" //true
+// let str2 = "Grandpa" //false
+// let str3 = "Anna" //true
+// let str4 = "MoPaR" //false
+// let str5 = "A man, a plan, a canal. Panama" //false?
+// // actually is true after replacing the characters we don't need
+// let str6 = "Radar" //true
+
+// console.log(checkPalindrome(str1));
+// console.log(checkPalindrome(str2));
+// console.log(checkPalindrome(str3));
+// console.log(checkPalindrome(str4));
+// console.log(checkPalindrome(str5));
+// console.log(checkPalindrome(str6));
+
+//////////////////////
+// Section 3
 //////////////////////
